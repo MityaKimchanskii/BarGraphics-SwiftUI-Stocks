68c7078c7c516683b3b23bb045e54d80fa355613
Dmitrii Kim <mitya_kim@icloud.com>
2022-07-22 00:35:46 -0700

    Initial Commit

---
+++ BarGraphics-SwiftUI-Stocks/BarGraphics_SwiftUI_StocksApp.swift
@@ -0,0 +1,17 @@
+//
+//  BarGraphics_SwiftUI_StocksApp.swift
+//  BarGraphics-SwiftUI-Stocks
+//
+//  Created by Mitya Kim on 7/22/22.
+//
+
+import SwiftUI
+
+@main
+struct BarGraphics_SwiftUI_StocksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
